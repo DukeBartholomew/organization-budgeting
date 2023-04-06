418ab7e3ccaa499bc9186e4505165033c2d18393
Ethan Chou <ethan.chou.2@gmail.com>
2023-04-06 14:54:47 -0500

    Reverted connection in connect.js to use .env file

--- backend/mysql/connect.js
+++ backend/mysql/connect.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection = createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'BeOrganized',
-  database: 'DBUI'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 }).promise();
 
 connection.connect((err) => {
