66d76ffb69e41ca5bcd621433c1f8da243cc3144
DukeBartholomew <78329199+DukeBartholomew@users.noreply.github.com>
2023-04-14 11:24:00 -0500

    Post Organization Implemented

--- frontend/src/components/OrgTextInput.jsx
+++ frontend/src/components/OrgTextInput.jsx
@@ -1,91 +1,49 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { TextInput, createStyles, rem } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 
+export function OrgTextInput() {
+  const url = "http://localhost:8000";
+  const navigate = useNavigate();
 
-const url = "http://localhost:8000";
-const useStyles = createStyles((theme, { floating }) => ({
-  root: {
-    position: "relative",
-    width: "50%",
-  },
+  const [orgName, setOrgName] = useState("");
 
-  label: {
-    position: "absolute",
-    zIndex: 2,
-    top: rem(7),
-    left: theme.spacing.sm,
-    pointerEvents: "none",
-    color: floating
-      ? theme.colorScheme === "dark"
-        ? theme.white
-        : theme.black
-      : theme.colorScheme === "dark"
-      ? theme.colors.dark[3]
-      : theme.colors.gray[5],
-    transition: "transform 150ms ease, color 150ms ease, font-size 150ms ease",
-    transform: floating
-      ? `translate(-${theme.spacing.sm}, ${rem(-28)})`
-      : "none",
-    fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
-    fontWeight: floating ? 500 : 400,
-  },
+  const handleOrgNameChange = (event) => {
+    setOrgName(event.target.value);
+  };
 
-  required: {
-    transition: "opacity 150ms ease",
-    opacity: floating ? 1 : 0,
-  },
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  input: {
-    "&::placeholder": {
-      transition: "color 150ms ease",
-      color: !floating ? "transparent" : undefined,
-    },
-  },
-}));
+    const requestData = {
+      orgName: orgName,
+      // Add any other relevant properties to the request body here
+    };
 
-
-const sendOrg = (org) => {
     axios
-      .post(url + "/orgs", org)
-      .then((res) => {
-        alert(JSON.stringify(res.data));
+      .post(url + "/orgs", requestData)
+      .then((response) => {
+        navigate("/home");
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error);
       });
   };
 
-export function OrgTextInput() {
-  const navigate = useNavigate();
-    
-  const [focused, setFocused] = useState(false);
-  const [value, setValue] = useState("");
-  const { classes } = useStyles({
-    floating: value.trim().length !== 0 || focused,
-  });
-
-  return <>
-    <TextInput
-      label="New Organization Name"
-      placeholder="Ex: ACME, Scott's Tots, Red Apple"
-      required
-      classNames={classes}
-      value={value}
-      onChange={(event) => setValue(event.currentTarget.value)}
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
-      mt="md"
-      autoComplete="nope"
-    />
-
-    <button type='button'
-            onClick={ () => {
-                sendOrg({
-                  value,
-                });
-                navigate('/home');
-              }}>Add</button>
-    </>
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="orgName">Organization Name:</label>
+      <input
+        type="text"
+        id="orgName"
+        name="orgName"
+        required
+        value={orgName}
+        onChange={handleOrgNameChange}
+      />
+      {/* Add any other relevant input fields here */}
+      <button type="submit">Create Organization</button>
+    </form>
+  );
 }
+
